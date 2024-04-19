78daf6aa549471b764b5a45e7a8eed8bbf936061
NgKoaz <nguyenkhoa220703@gmail.com>
2024-04-19 11:27:22 +0700

    Init2, mqtt library, register firebase.

---
+++ App/app/src/main/java/bku/iot/farmapp/view/pages/SignOutActivity.java
@@ -0,0 +1,16 @@
+package bku.iot.farmapp.view.pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import bku.iot.farmapp.R;
+
+public class SignOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_out);
+    }
+}
