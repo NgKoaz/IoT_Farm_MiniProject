2f516a2cf57e079c530918e2586ed5d72f346987
manhtrannnnnn <manhdeptrainhat2003@gmail.com>
2024-04-20 20:41:28 +0700

    Signup UI

--- App/app/src/main/java/bku/iot/farmapp/view/pages/SignUpActivity.java
+++ App/app/src/main/java/bku/iot/farmapp/view/pages/SignUpActivity.java
@@ -2,7 +2,10 @@ package bku.iot.farmapp.view.pages;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import bku.iot.farmapp.R;
 
@@ -12,5 +15,12 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        TextView btn = findViewById(R.id.alreadyHaveAccount);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+            }
+        });
     }
 }
