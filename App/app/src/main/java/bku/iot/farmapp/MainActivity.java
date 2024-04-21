aa5054144727b47611f724dabb8b21ab0486a8af
NgKoaz <nguyenkhoa220703@gmail.com>
2024-04-21 08:56:19 +0700

    UI is Done...

--- App/app/src/main/java/bku/iot/farmapp/MainActivity.java
+++ App/app/src/main/java/bku/iot/farmapp/MainActivity.java
@@ -2,13 +2,26 @@ package bku.iot.farmapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import bku.iot.farmapp.view.pages.SignInActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final long SPLASH_SCREEN_DELAY = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(this, SignInActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_SCREEN_DELAY);
     }
 }
